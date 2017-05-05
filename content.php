5f70606bfd9fd5f8a9d102250bcd6ae7a5ca62ab
Alvar Sirlin <alvar@alvarsirlin.com>
2017-05-05 12:38:02 -0400

    added files excluded under foundation gitignore, datepicker current day, calenders toggle

--- content.php
+++ content.php
@@ -3,20 +3,24 @@
 Drew Windell <span>Last Race, April 15, 2017</span></h1>
 
 <!--
-.row>.large-12|c
+.row>.columns.large-12|c
 -->
 
 
 <div class="row">
-	<div class="large-12">
+	<div class="columns large-12 show-for-medium">
 		<div id="datepicker"></div>
 	</div><!-- /.large-12 -->
 </div><!-- /.row -->
-
-
 <div class="row">
-	<div class="large-12 date-title">
-		<h2>April 12, 2017 <a href="" class='cal'><img src="images/cal.png" alt="" width='25'></a></h2>
+	<div class="columns large-12 date-title">
+	<div class="row">
+		<div class="columns large-2 date-nav"><span><a class="prev" href=""><img src="images/arrow-prev.png" width='16' alt=""></a></span></div>
+		<div class="columns large-8">
+			<h2>April 12, 2017 <img class='cal' src="images/cal.png" alt="" width='25'></h2>
+		</div>
+		<div class="columns large-2 date-nav text-right"><a class="next" href=""><img src="images/arrow-next.png" width='16' alt=""></a></div>
+	</div>
 	</div><!-- /.large-12 -->
 </div><!-- /.row -->
 
--- js/app.js
+++ js/app.js
@@ -1,31 +1,41 @@
 $(document).foundation();
 
 var events = [ 
-    { Title: "Race1", Date: new Date("04/13/2017") }, 
-    { Title: "Race2", Date: new Date("05/25/2017") }, 
-    { Title: "Race3", Date: new Date("06/01/2017") }
+    { Athlete: "Drew Windell", Title: "Race1",  Date: new Date("04/13/2017") }, 
+    { Athlete: "Drew Windell", Title: "Race2", Date: new Date("04/25/2017") }, 
+    { Athlete: "Drew Windell", Title: "Race3", Date: new Date("06/01/2017") },
+    //{ Athlete: "Hannah Fields", Title: "Race1",  Date: new Date("04/11/2017") }, 
+    //{ Athlete: "Hannah Fields", Title: "Race2", Date: new Date("04/22/2017") }, 
+    //{ Athlete: "Hannah Fields", Title: "Race3", Date: new Date("06/13/2017") }
 ];
 
 
-$("#datepicker .ui-datepicker-inline").removeAttr("style");
- $( function() {
-    $( "#datepicker" ).datepicker({
-		numberOfMonths: 3,
-		defaultDate: "-1m", 
 
-	 beforeShowDay: function(date) {
+
+ $( function() {
+
+	$(".cal").click(function() {
+	  $( "#datepicker" ).slideToggle( "slow");
+	 });
+
+
+	$("#datepicker .ui-datepicker-inline").removeAttr("style");
+
+   $( "#datepicker" ).datepicker({
+		defaultDate: $.datepicker.parseDate("d m y", "12 4 17"),
+		numberOfMonths: 3,
+
+	 	beforeShowDay: function(date) {
         var result = [true, '', null];
         var matching = $.grep(events, function(event) {
             return event.Date.valueOf() === date.valueOf();
         });
-        
         if (matching.length) {
             result = [true, 'highlight', null];
         }
         return result;
-    },
-
-    onSelect: function(dateText) {
+     },
+     onSelect: function(dateText) {
         var date,
             selectedDate = new Date(dateText),
             i = 0,
@@ -40,11 +50,11 @@ $("#datepicker .ui-datepicker-inline").removeAttr("style");
             i++;
         }
         if (event) {
-            //alert(event.Title);
 				window.location.replace('index.php?var=' + event.Title);
         }
     }
 	 });
+
   } );
 
 
