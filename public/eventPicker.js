a70e895040ae40b856b071023ce3945fb157bcb2
Mauro Restuccia <mrestuccia@mac.com>
2017-05-08 07:03:46 -0400

    datepicker changing btw athlete

--- public/eventPicker.js
+++ public/eventPicker.js
@@ -8,11 +8,12 @@ function eventPicker(containerId, athleteList, selected) {
   })[0];
 
 
-  function click(){
-    console.log('click');
-  }
+  var events = selectedAthete.events;
+  var event = selectedAthete.events[0];
 
-  // Return the container
+  console.log('first event', event);
+
+  // Return the container //April 12, 2017
   var renderContainer = $(`
     <div class="row">
 	    <div class="columns large-12 date-title">
@@ -23,7 +24,7 @@ function eventPicker(containerId, athleteList, selected) {
             </span>
           </div>
 		      <div class="columns large-8">
-			      <h2>April 12, 2017 <img class='cal' src="images/cal-2x.png" alt="" width='25'></h2>
+			      <h2>${event.Date.toDateString('d m, yy').slice(4)} <img class='cal' src="images/cal-2x.png" alt="" width='25'></h2>
 		      </div>
 		      <div class="columns large-2 date-nav text-right">
             <a class="next" href="" onClick='click()'><img src="images/arrow-next-2x.png" width='16' alt=""></a>
