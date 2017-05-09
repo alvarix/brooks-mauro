9b95fec9610161eeaa6d36e0a770779ac4405e18
Alvar Sirlin <alvar@alvarsirlin.com>
2017-05-09 13:39:15 -0400

    fix date-title arrows

--- public/eventPicker.js
+++ public/eventPicker.js
@@ -15,17 +15,17 @@ function eventPicker(containerId, athleteList, selected, eventDate) {
   // Return the container //April 12, 2017
   var renderContainer = $(`
     <div class="row">
-	    <div class="columns large-12 date-title">
+	    <div class="columns small-12 date-title">
 	      <div class="row">
-		      <div class="columns large-2 date-nav">
+		      <div class="columns small-2 date-nav">
             <span>
               <a class="prev" href=""><img src="images/arrow-prev-2x.png" width='16' alt=""></a>
             </span>
           </div>
-		      <div class="columns large-8">
+		      <div class="columns small-8">
 			      <h2>${eventDate.toDateString('d m, yy').slice(4)} <img class='cal' src="images/cal-2x.png" alt="" width='25'></h2>
 		      </div>
-		      <div class="columns large-2 date-nav text-right">
+		      <div class="columns small-2 date-nav text-right">
             <a class="next" href="" onClick='click()'><img src="images/arrow-next-2x.png" width='16' alt=""></a>
           </div>
 	      </div>
